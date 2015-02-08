59216a22d484da52f3034d52d7a69c2017003125
Armin Buescher <armin.buescher@googlemail.com>
2015-02-07 16:28:10 -0800

    Fixed de-duplication

--- ioc-parser.py
+++ ioc-parser.py
@@ -52,9 +52,9 @@ class IOC_Parser(object):
     patterns = {}
 
     def __init__(self, args):
-        self.files     = args.PDF
-        self.format    = self.check_format(args.FORMAT)
-        self.dd        = set() if args.DEDUP else None
+        self.files = args.PDF
+        self.format = self.check_format(args.FORMAT)
+        self.dedup = args.DEDUP
         self.whitelist = WhiteList()
 
         self.load_patterns(args.INI)
@@ -88,40 +88,40 @@ class IOC_Parser(object):
 
         return False
 
-    def parse_page(self, page, page_num, fpath):
-        data = page.extractText()
-
-        for ind_type, ind_regex in self.patterns.iteritems():
-            matches = ind_regex.findall(data)
-
-            for ind_match in matches:
-                if isinstance(ind_match, tuple):
-                    ind_match = ind_match[0]
-
-                if self.is_whitelisted(ind_match, ind_type):
-                    continue
-
-                if self.dd:
-                    if (ind_type, ind_match) in self.dd:
-                        continue
-
-                    self.dd.add((ind_type, ind_match))
-
-                self.print_match(fpath, page_num, ind_type, ind_match)
-
     def parse_file(self, fpath):
         with open(fpath, 'rb') as f:
             try:
                 pdf = PdfFileReader(f, strict = False)
 
+                if self.dedup:
+                    dd = set()
+
                 page_num = 0
                 for page in pdf.pages:
                     page_num += 1
-                    self.parse_page(page, page_num, fpath)
+                    data = page.extractText()
+
+                    for ind_type, ind_regex in self.patterns.iteritems():
+                        matches = ind_regex.findall(data)
+
+                        for ind_match in matches:
+                            if isinstance(ind_match, tuple):
+                                ind_match = ind_match[0]
+
+                            if self.is_whitelisted(ind_match, ind_type):
+                                continue
+
+                            if self.dedup:
+                                if (ind_type, ind_match) in dd:
+                                    continue
+
+                                dd.add((ind_type, ind_match))
+
+                            self.print_match(fpath, page_num, ind_type, ind_match)
             except (KeyboardInterrupt, SystemExit):
                 raise
-            except:
-                self.print_error(fpath, traceback.format_exc())
+            except Exception, e:
+                self.print_error(fpath, e)
 
     def parse(self):
         if os.path.isfile(args.PDF):
