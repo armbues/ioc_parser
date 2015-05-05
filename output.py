cb7593689e7f444c287d1d428a3c6f1c02278993
MrAdz350 <MrAdz350@users.noreply.github.com>
2015-05-05 22:40:12 +0100

    Revert "Remove Bro File Header addition"
    This reverts commit d7e754d305555d1d2486fb0f21267d945faa4279.

--- output.py
+++ output.py
@@ -108,7 +108,10 @@ class OutputHandler_bro(OutputHandler):
             "Filename": "Intel::FILE_NAME",
         })
 
-    def print_match(self, fpath, name, match):
+    def print_header(self, fpath):
+        print("#fields indicator\tindicator_type\tmeta.source\tmeta.url\tmeta.do_notice\tmeta.if_in")
+    
+    def print_match(self, fpath, page, name, match):
         source_name = os.path.splitext(os.path.basename(fpath))[0].translate(self.rule_enc)
         
         if name in self.intel_types:
