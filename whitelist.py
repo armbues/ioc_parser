4fac70fa445d35f2aeddd7c01d6f9461fb838a17
ttufts <thomas.j.tufts@gmail.com>
2015-06-30 16:07:26 -0400

    Didn't allow underscore in path to whitelist folder
    Previously the split on underscore was done on the entire path, so if
    there was an underscore earlier in the path, it would break the parsing
    for the whitelist type. This splits the path first so you are only
    splitting on the whitelist filename.

--- whitelist.py
+++ whitelist.py
@@ -7,6 +7,6 @@ class WhiteList(dict):
     	searchdir = os.path.join(basedir, "whitelists/whitelist_*.ini")
         fpaths = glob.glob(searchdir)
         for fpath in fpaths:
-            t = os.path.splitext(fpath)[0].split('_',1)[1]
+            t = os.path.splitext(os.path.split(fpath)[1])[0].split('_',1)[1]
             patterns = [line.strip() for line in open(fpath)]
             self[t]  = [re.compile(p) for p in patterns]
