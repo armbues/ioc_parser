7c960601914b172a2751dfdf0e970359fbb2364f
ttufts <thomas.j.tufts@gmail.com>
2015-07-09 17:25:11 -0400

    Pass whitelist dict into WhiteList constructor
    Adds the capability to create a new WhiteList object from a dict of
    format:
    
    {
    “Host”: “example.com$”,
    “URL”: “^http:\/\/www.fireeye.com\/“
    }

--- whitelist.py
+++ whitelist.py
@@ -3,11 +3,16 @@ import glob
 import re
 
 class WhiteList(dict):
-    def __init__(self, basedir):
-    	searchdir = os.path.join(basedir, "whitelists/whitelist_*.ini")
-        fpaths = glob.glob(searchdir)
-        for fpath in fpaths:
-            t = os.path.splitext(os.path.split(fpath)[1])[0].split('_',1)[1]
-            with open(fpath) as f:
-                patterns = [line.strip() for line in f]
-                self[t]  = [re.compile(p) for p in patterns]
+    def __init__(self, basedir=None, whitelist_dict=None):
+        if basedir is not None:
+            searchdir = os.path.join(basedir, "whitelists/whitelist_*.ini")
+            fpaths = glob.glob(searchdir)
+            for fpath in fpaths:
+                t = os.path.splitext(os.path.split(fpath)[1])[0].split('_',1)[1]
+                with open(fpath) as f:
+                    patterns = [line.strip() for line in f]
+                    self[t]  = [re.compile(p) for p in patterns]
+
+        if whitelist_dict is not None:
+            for t in whitelist_dict:
+                self[t] = [re.compile(p) for p in whitelist_dict[t]]
