3f2304e47205922cb88c5dea14364e6ee6dc6ada
Scot Matson <sjpmatson@gmail.com>
2016-03-16 14:31:47 -0700

    Changed tab into space. Fixed TabError.

--- whitelist.py
+++ whitelist.py
@@ -4,7 +4,7 @@ import re
 
 class WhiteList(dict):
     def __init__(self, basedir):
-    	searchdir = os.path.join(basedir, "whitelists/whitelist_*.ini")
+        searchdir = os.path.join(basedir, "whitelists/whitelist_*.ini")
         fpaths = glob.glob(searchdir)
         for fpath in fpaths:
             t = os.path.splitext(os.path.split(fpath)[1])[0].split('_',1)[1]
