f770409cb8f21bf2daeb8b580a92f56417a6c4ba
Angelo Dell'Aera <buffer@olografix.org>
2017-06-21 16:49:44 +0200

    Multiple improvements
    	- Code layout changes (PEP-8 coding style)
    	- Python 3 compatibility
    	- Parser class initialization refactoring
    	- Compile the regexps with the IGNORECASE flag

--- setup.py
+++ setup.py
@@ -1,28 +1,22 @@
 #!/usr/bin/env python
 
-import os
 from setuptools import setup
 
 setup(
-	name = "ioc_parser",
-	version = "0.9.1",
-	author = "Armin Buescher",
-	author_email = "armin.buescher@googlemail.com",
-	scripts=['bin/iocp'],
-	description = ("Tool to extract indicators of compromise from security reports"),
-	license = "MIT",
-	url = "https://github.com/armbues/ioc_parser",
-	packages=['iocp'],
-	include_package_data=True,
-	classifiers=[
-		"Development Status :: 4 - Beta",
-		"Topic :: Security",
-		"License :: OSI Approved :: MIT License",
-	],
-	install_requires=[
-		"pdfminer",
-		"PyPDF2",
-		"requests",
-		"beautifulsoup4"
-	],
+    name = "ioc_parser",
+    version = "0.9.1",
+    author = "Armin Buescher",
+    author_email = "armin.buescher@googlemail.com",
+    scripts = ['bin/iocp'],
+    description = ("Tool to extract indicators of compromise from security reports"),
+    license = "MIT",
+    url = "https://github.com/armbues/ioc_parser",
+    packages = ['iocp'],
+    include_package_data = True,
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Topic :: Security",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires= open("requirements.txt").read().splitlines(),
 )
